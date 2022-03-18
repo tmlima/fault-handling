7ddd932fe7d211fffdc2144f1aa7110628a072ac
tmlima <thiago.lima@warren.com.br>
2022-03-17 22:50:10 -0300

    solution and faulty service

---
+++ src/FaultyService/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaultyService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : Controller
+    {
+        private readonly ILogger<WeatherController> _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeather")]
+        public IActionResult GetWeather()
+        {
+            int value = new Random().Next(5);
+            if (value == 0)
+            {
+                _logger.LogError("error");
+                return StatusCode(500);
+            }
+
+            _logger.LogInformation("ok");
+            return Ok("overcast");
+        }
+    }
+}
